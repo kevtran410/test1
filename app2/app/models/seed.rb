6ea1614fed8403af0f7cc64763cc4c3846c6fc84
Tran Ngoc Nam <kevtran.410@gmail.com>
2018-06-27 08:39:24 +0700

    Fix app combine

--- app2/app/models/seed.rb
+++ app2/app/models/seed.rb
@@ -4,5 +4,6 @@ class Seed < ApplicationRecord
   scope :random_non_consumed, ->(label_prefix){where(consumed: false)
     .where("label LIKE ?", "#{label_prefix}%").order("RANDOM()").limit(1).first}
 
-  scope :current_10, ->{where("label LIKE ?", "#{label_prefix}%").order("id DESC").limit(10)}
+  scope :current_10, ->(label_prefix){where("label LIKE ?", "#{label_prefix}%")
+    .order("id DESC").limit(10)}
 end
